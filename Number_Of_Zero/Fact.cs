df978cf92e63451a3a3400a72860a0b8fa6bb4a4
Suraj Narayan <snarayan@tavisca.com>
2017-07-10 20:22:46 +0530

    initial commit

---
+++ Number_Of_Zero/Fact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfZero
+{
+    public class Fact
+    {
+       public int Factoriallogic(double num,double bases)
+        {
+            double dec=0, i=0;
+            int number = (int)num;
+            while(number>0)
+            {
+                //Console.WriteLine("num= "+num);
+                double r = number % 10;
+                dec = dec +( (int)Math.Pow(bases, i) )* r;
+                i++;
+                number = number / 10;
+            }
+            //Console.WriteLine(dec);
+             int Counter = 0;
+             for(i=1;i<=dec;i++)
+             {
+                 double temp = i;
+
+                 while (temp % 5==0)
+                 { 
+                     Counter++;
+                     temp = temp / 5;
+                 }
+
+             }
+             return Counter;
+            
+
+        }
+
+
+
+    }
+}
---
+++ program.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumberOfZero;
+
+namespace program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Base");
+
+            double bases = Convert.ToDouble(Console.ReadLine());
+
+            Fact factorial = new Fact();
+            int num_Zero = factorial.Factoriallogic(num, bases);
+            Console.WriteLine("The number of zero is " + num_Zero);
+
+            Console.ReadKey();
+        }
+    }
+}
